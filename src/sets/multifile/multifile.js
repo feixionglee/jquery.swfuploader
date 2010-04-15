cf76e82c7aaac810df34ca74b3cfa2a5ad810cab
Aaron Qian <aq1018@gmail.com>
2010-04-16 00:11:18 +0800

    setting up MultiFile for more coding

--- src/sets/multifile/multifile.js
+++ src/sets/multifile/multifile.js
@@ -1,5 +1,40 @@
 (function($){
 	$.SwfUploader.sets.MultiFile = function(swfuploader){
+		$.SwfUploader.sets.AbstractSet.apply(this, arguments);
+	};
+	
+	$.SwfUploader.sets.MultiFile.prototype = $.extend($.SwfUploader.sets.AbstractSet.prototype, {	
+		init : function() {
+			this.setButtonPlaceholder(this.$el);
+		},
 
-	}
+  	fileDialogComplete: function(numFilesSelected, numFilesQueued, totalFilesInQueue) {
+  	},
+  	
+  	fileQueued : function(file){
+  	},
+  	
+  	fileQueueError : function(file, errorCode, message){
+  	},
+  	
+  	queueComplete : function(numFilesUploaded){
+  	},
+  	
+  	uploadStart : function(file){
+  	},
+  	
+  	uploadProgress: function(file, bytesLoaded, bytesTotal) {
+  	},
+  	
+  	uploadSuccess: function(file, serverData, response) {			
+  	},
+  	
+  	uploadComplete : function(file){
+  	},
+  	
+  	uploadError: function(file, errorCode, message) {
+  	}
+
+	});	
+	
 })(jQuery);
